59ac7278a4e82e7ebce97800eac74970bf694a5f
funkytek <funkytek@trigram.com>
2021-02-11 17:17:57 -0800

    update app

--- src/ui/views/App.tsx
+++ src/ui/views/App.tsx
@@ -26,13 +26,25 @@ function App({}: AppProps) {
 
   return (
     <VBox center vcenter className='bg-black min-w-full min-h-screen'>
-      <Heading size={1} color={'gray-800'}>TRIGRAM-TEMPLATE-LITE</Heading>
-
-      <br /><br />
 
       <div className='text-9xl text-white transition-all'>{TRIGRAMS[count]}</div>
 
       <br /><br />
+
+      <Heading size={1} color={'gray-500'}>TRIGRAM-TEMPLATE-LITE</Heading>
+
+      <br /><br />
+
+      <ul className='text-gray-300 text-xl'>
+        <li>- Snowpack dev server</li>
+        <li>- React</li>
+        <li>- Typescript</li>
+        <li>- Tailwind - configured via postcss</li>
+        <li>- @ alias for src/</li>
+        <li>- Opinionated view structure</li> 
+      </ul>
+
+      <br /><br /><br />
 
       <Heading size={1} color={'white'}>Header - 1</Heading>
       <Heading size={2} color={'white'}>Header - 2</Heading>
